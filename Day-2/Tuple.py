7c72820663359cd6e2d1ef2a05c98ead85c5c7d4
manoharraj07 <manoharbandela5@gmail.com>
2026-06-18 09:43:45 +0000

    commit all

---
+++ Day-2/Tuple.py
@@ -0,0 +1,7 @@
+Branch='CSO','CSD','CSE'
+first=Branch[0]
+second=Branch[1]
+third=Branch[2]
+print(first)
+print(second)
+print(third)
---
+++ Day-2/string operations.py
@@ -0,0 +1,13 @@
+name1="ACE "
+name2="ENGINEERING COLLEGE"
+name3=name1+name2
+print(name3)
+name='''ace
+engineering
+college'''
+print(name)
+print(name3[0:3])
+print(name3[0:16])
+print(name3[4])
+print(name3[:])
+print(len(name3))
